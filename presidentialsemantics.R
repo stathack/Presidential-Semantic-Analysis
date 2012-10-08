3d1d9814e65586ba6c696be79de5c64b425ec9e0
stathack <chris@kwelia.com>
2012-10-07 22:23:50 -0400

    fix graphing and add mean line

--- presidentialsemantics.R
+++ presidentialsemantics.R
@@ -9,11 +9,16 @@ president<-function(x){
     textRomney=laply(Romney, function(t) t$getText())
     textObama=laply(Obama, function(t) t$getText())
     resultRomney=score.sentiment(textRomney, positive.words, negative.words)
+    resultRomney$candidate='Romney'
     resultObama=score.sentiment(textObama, positive.words, negative.words)
-    output<-data.frame(Romney=resultRomney$score, Obama=resultObama$score)
-    out<- melt(output)
-    p<-ggplot(out)+ geom_density(aes(x = value,colour = variable))
+    resultObama$candidate='Obama'
+    result<-merge(resultObama,resultRomney, all=TRUE)
+    result$candidate<-as.factor(result$candidate)
+    cresult<-ddply(result, .(candidate),summarise, score.mean=mean(score))
+    p<-ggplot(result, aes(x=score, fill=candidate)) +
+        geom_histogram(binwidth=.5, alpha=.5, position="identity") +
+        geom_vline(data=cresult, aes(xintercept=score.mean,  colour=candidate),
+                   linetype="dashed", size=1)
     print(p)
-    print(mean(resultObama$score))
-    print(mean(resultRomney$score))
+    return(cresult)
 }
